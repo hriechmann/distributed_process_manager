06857d9b2f0eb9c294c87095412df88ce4a3cd39
Hannes Riechmann <hriechmann@web.de>
2014-07-31 18:25:22 +0200

    added small class for file handling

--- src/eu/hriechmann/distributed_process_manager/common.py
+++ src/eu/hriechmann/distributed_process_manager/common.py
@@ -1,6 +1,8 @@
 __author__ = 'hriechma'
 
 from enum import Enum
+import hashlib
+import os
 
 ManagerCommands = Enum("ManagerCommands", "REGISTER INIT_PROCESS START_PROCESS STOP_PROCESS SEND_LOGS")
 ClientCommands = Enum("ClientCommands", "REGISTER PROCESSSTATUS_CHANGED PROCESS_LOGS")
@@ -41,3 +43,34 @@ class ProcessStatus(object):
     def __str__(self):
         return "ProcessStatues: Process: "+str(self.process_desc)+" on client: "+\
                str(self.responsible_client)+" has status: "+self.status
+           
+           
+BLOCKSIZE = 65536
+MAX_SIZE = 1024*1024*100
+
+class IncrementalFileReaderAndHasher(object):
+    
+    def __init__(self, filename):
+        self.hasher = hashlib.sha1()
+        self.filename = filename
+        statinfo = os.stat(self.filename)
+        self.big_file = statinfo > MAX_SIZE
+        if self.big_file:
+            self.file_contents = None
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        else:
+            afile = open(self.filename,'ab')
+            self.file_contents = afile.read()
+            self.hasher.update(self.file_contents)
+        
+    def hash(self):
+        return self.hasher.hexdigest()
+        
+    def get_file_contents(self):
+        if self.big_file:
+            TODO
+        else:
+            return self.file_contents
